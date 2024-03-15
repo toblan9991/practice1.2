eba8e539fa1e4415bc06ceb177a534c3ee2fb36e
toblan9991 <oluwatobiodulana@gmail.com>
2024-03-14 18:18:16 -0700

    feat: added live hosting code

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
+// New
+const path = require("path");
 
 const router = require("./routes");
 require("./models/db");
@@ -9,6 +11,13 @@ require("./models/db");
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "../frontend/dist"))); //new
+
+// New
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
 
 app.use("/api/v1", router);
 
